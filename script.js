3b0e2b98cd3013748ebd616f1cd370c1e06a01fd
designerlouise <designlouisegrafico@gmail.com>
2025-07-14 19:08:45 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    const destino = document.querySelector(this.getAttribute('href'));
-    if (destino) {
-      destino.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
