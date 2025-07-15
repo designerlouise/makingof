4e806ba4096e237176d06604f4047fd3a48e3ff1
designerlouise <designlouisegrafico@gmail.com>
2025-07-15 20:36:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.querySelectorAll('a[href^="#"]').forEach(a => {
-  a.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+document.querySelectorAll('.faq-question').forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('active');
+    const answer = btn.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
   });
 });
