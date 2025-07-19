8cc2768a76fe0b79a41310a45fa54f04e1b23d50
designerlouise <designlouisegrafico@gmail.com>
2025-07-19 13:10:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// PAUSA O CARROSSEL AO PASSAR O MOUSE
 const track = document.querySelector('.carousel-track');
 
 track.addEventListener('mouseover', () => {
@@ -6,4 +7,14 @@ track.addEventListener('mouseover', () => {
 
 track.addEventListener('mouseout', () => {
   track.style.animationPlayState = 'running';
+});
+
+// EXPANDE/RETRAI FAQ
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const question = item.querySelector(".faq-question");
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
 });
